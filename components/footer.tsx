6024555101ef4c7266f44a0ebc26caa24c106281
rfrlcode <info@rfrlcode.com>
2023-11-21 14:55:43 -0800

    added footer

---
+++ components/footer.tsx
@@ -0,0 +1,16 @@
+"use client"
+import Container from "./ui/container"
+
+const Footer = () => {
+    return (
+    <header>
+        <Container>
+        <div className="flex items-center justify-between py-12">
+
+        </div>
+        </Container>
+    </header>
+  )
+}
+
+export default Footer;
