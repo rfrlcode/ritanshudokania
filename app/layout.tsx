7a8de7ccf74304fa30e450d9d99301c2e31c9268
rfrlcode <info@rfrlcode.com>
2024-02-18 12:25:07 -0800

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Analytics } from "@vercel/analytics/react";
 
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
@@ -6,19 +7,19 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-        {children}
-      </ThemeProvider>
-        </body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+          <Analytics />
+        </ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
